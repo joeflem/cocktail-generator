b3d938fed712f82a106c2f0bfaaa2eba9f354956
Joe Ciavucco <joe.ciavucco@digitalbalance.co.uk>
2020-04-13 18:32:55 +0100

    Added style structure, fonts, colours and started generator

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="generator">
+      <slot v-if="!started">
+
+      </slot>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        data: function () {
+            return {
+              started: false,
+            };
+        }
+    }
 </script>
---
+++ src/views/Privacy.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="privacy">
+        Privacy
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Privacy',
+        components: {
+
+        }
+    }
+</script>
